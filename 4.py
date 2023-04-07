ce584a75753eed5e91b183786c27527256ec1859
sejal <sejalvishnoi32@gmail.com>
2023-04-07 20:11:57 +0530

    This is my lists and tp codes in python programming language

---
+++ 4.py
@@ -0,0 +1,26 @@
+# This code about lists & tp 
+grocery = ["sejal","toffi","moje","cream"]
+print(grocery[0])
+Number = [2,8,0,78,76,56,56]
+print(Number)
+Number.reverse()
+print(Number)
+Number.sort()
+print(Number)
+print(Number[0:4])
+print(max(Number))
+print(min(Number))
+Number.append(10)
+Number.insert(2,20)
+print(Number)
+Number.remove(0)
+print(Number)
+Number.pop()
+print(Number)
+# mutable = can change
+# immutable = cannot change
+# swap the two numbers in python programming language
+a = 2
+b = 6
+a,b = b,a
+print(a,b)
